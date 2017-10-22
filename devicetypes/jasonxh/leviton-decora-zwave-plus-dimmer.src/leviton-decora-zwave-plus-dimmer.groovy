0ff32b2e80046a49045afdd10a61657510cd3acb
Brian Scott <brian@whoisbrian.com>
2017-10-22 10:23:55 -0500

    blah

--- devicetypes/jasonxh/leviton-decora-zwave-plus-dimmer.src/leviton-decora-zwave-plus-dimmer.groovy
+++ devicetypes/jasonxh/leviton-decora-zwave-plus-dimmer.src/leviton-decora-zwave-plus-dimmer.groovy
@@ -80,8 +80,8 @@ metadata {
         standardTile("low", "device.level", width: 2, height: 2, inactiveLabel: false, decoration: "flat") {
             /*state "default", label: 'LOW', action: "low", icon: "st.Lighting.light14"*/
 			state "default", label: 'LOW', action: "low", icon:"st.Home.home30", backgroundColor: "#ffffff"
-			state "LOW", label:'LOW', action: "low", icon:"st.Home.home30", backgroundColor: "#79b821"
-			state "ADJUSTING.LOW", label:'LOW', action: "low", icon:"st.Home.home30", backgroundColor: "#2179b8", nextState:"LOW"
+			state "1..10", label:'LOW', action: "low", icon:"st.Home.home30", backgroundColor: "#79b821"
+			state "ADJUSTING.LOW", label:'LOW', action: "low", icon:"st.Home.home30", backgroundColor: "#2179b8"
         }
 
         standardTile("medium", "device.level", width: 2, height: 2, inactiveLabel: false, decoration: "flat") {
